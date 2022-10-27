e3f84a63cda75e15448b9b2229dad43578d0a0b9
Suman Paik <sumanpaik.dev@gmail.com>
2022-10-27 15:02:52 +0530

    another figma design added, not finished yet but working in progress

---
+++ items-description/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./App.css"
+
+export default function App() {
+  return(
+    <div className="app--container">
+      <h1>Figma file to React Dev</h1>
+    </div>
+  );
+}
+
